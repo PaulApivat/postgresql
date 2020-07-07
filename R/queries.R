c0ce097eb93b0c6b4d7a7dd6f0ea75c318d713dd
paulapivat <paul.apivat@gmail.com>
2020-07-07 12:22:39 +0700

    add select distinct

--- R/queries.R
+++ R/queries.R
@@ -1,9 +1,56 @@
 # R Queries in SQL ----
 
 # Libraries ----
+library(tidyverse)
 library(dbplyr)
 
 # SQL Statement Fundamentals ----
+
+# SELECT * FROM table; 
+# SELECT columns FROM table;
+# SELECT col1, col2 FROM table;
+# SELECT DISTINCT(column) FROM table;
+
+# Challenge: SELECT
+# Business Situation: send promotional email to all existing customers
+# SELECT first_name, last_name, email  FROM customer;
+translate_sql(customer %>% select(first_name, last_name, email))
+
+# In order to demonstrate sql_translations, 
+# need to make TEMPORARY database with a couple of tables
+con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+flights <- copy_to(con, nycflights13::flights)
+airports <- copy_to(con, nycflights13::airports)
+
+# <SQL>
+# SELECT `dep_delay`, `arr_delay`
+# FROM `nycflights13::flights`
+flights %>%
+    select(contains("delay")) %>%
+    show_query()
+
+# <SQL>
+# SELECT `year`, `month`, `day`, `dep_time`, `sched_dep_time`, `dep_delay`, `arr_time`, `sched_arr_time`, `arr_delay`, `carrier`, `flight`, `tailnum`, `origin`, `dest`, `air_time`, `distance`, `hour`, `minute`, `time_hour`
+# FROM `nycflights13::flights`
+flights %>% 
+    select(everything()) %>% 
+    show_query()
+
+# DISTINCT
+# SELECT DISTINCT(release_year) FROM film;
+film %>%
+    distinct(release_year)
+
+# <SQL>
+# SELECT DISTINCT `month`
+# FROM `nycflights13::flights`
+flights %>%
+    distinct(month) %>%
+    show_query()
+
+
+
+
 
 # GROUP BY Statements ----
 
