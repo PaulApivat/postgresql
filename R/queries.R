9a2db70b739836063aa751dd5018171c95c14abc
paulapivat <paul.apivat@gmail.com>
2020-07-07 11:39:55 +0700

    add file structure and section headings to queries.R

--- R/queries.R
+++ R/queries.R
@@ -1,3 +1,26 @@
-# R Queries in SQL
+# R Queries in SQL ----
 
+# Libraries ----
 library(dbplyr)
+
+# SQL Statement Fundamentals ----
+
+# GROUP BY Statements ----
+
+# Assessment Test 1 ----
+
+# JOINS ----
+
+# Advanced SQL ----
+
+# Assessment Test 2 ----
+
+# Creating Databases and Tables ----
+
+# Assessment Test 3 ----
+
+# Conditional Expressions and Procedures ----
+
+# Extra: PostGreSQL with Python ----
+
+# Bonus ----
