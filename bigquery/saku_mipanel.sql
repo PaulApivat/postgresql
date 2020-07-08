9084881e702ac513563c77243a2ca3990df90987
paulapivat <paul.apivat@gmail.com>
2020-07-08 14:03:35 +0700

    add bigquery directory with saku_mipanel.sql

---
+++ bigquery/saku_mipanel.sql
@@ -0,0 +1,8 @@
+# mixpanel_engage
+
+SELECT * FROM `jobsbot-276604.saku_mixpanel.mixpanel_engage` 
+
+# How many unique users have engaged with jobsbot? (160)
+
+SELECT COUNT(DISTINCT key) 
+FROM `jobsbot-276604.saku_mixpanel.mixpanel_engage` 
