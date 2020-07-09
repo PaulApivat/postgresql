b5507f7d8ee9a7eaf4c8704c86413d8c96999203
paulapivat <paul.apivat@gmail.com>
2020-07-09 12:26:42 +0700

    complete Assessment Test 1

--- R/queries.R
+++ R/queries.R
@@ -623,6 +623,28 @@ HAVING SUM(amount) > 100
 
 # Assessment Test 1 ----
 
+# 1. Return customer IDs for those who spent at least $110 with staff member ID 2
+SELECT customer_id, staff_id, SUM(amount) FROM payment
+WHERE staff_id != 1
+GROUP BY staff_id, customer_id
+HAVING SUM(amount) >= 110
+
+# 2. How many films begin with the letter 'J' ?
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'J%';
+
+# 3. What customer has the highest customer ID number whose name starts with an 
+# 'E' and has an address ID lower than 500?
+SELECT first_name, last_name, customer_id, address_id FROM customer
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1
+
+
+
+
+
+
 # JOINS ----
 
 # Advanced SQL ----
