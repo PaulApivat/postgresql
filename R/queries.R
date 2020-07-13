93e8e5755bbca2c85aa83b6e1281cabb8b32299e
paulapivat <paul.apivat@gmail.com>
2020-07-13 15:52:06 +0700

    add assessment 3

--- R/queries.R
+++ R/queries.R
@@ -1453,13 +1453,57 @@ VALUES
 
 
 
-
-
-
-
-
-
 # Assessment Test 3 ----
+
+# Create a new database called "School" this database should have two tables: teachers and students.
+
+# The students table should have columns for student_id, first_name,last_name, homeroom_number, phone,email, and graduation year.
+CREATE TABLE students(
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    homeroom_number SMALLINT NOT NULL,
+    phone BIGINT UNIQUE NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL,
+    graduation_year SMALLINT NOT NULL
+)
+
+# The teachers table should have columns for teacher_id, first_name, last_name, homeroom_number, department, email, and phone.
+CREATE TABLE teachers(
+    teacher_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    homeroom_number SMALLINT NOT NULL,
+    department VARCHAR(200) NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL,
+    phone BIGINT UNIQUE NOT NULL
+)
+
+# Insert a student named Mark Watney (student_id = 1), phone number 777-555-1234, 
+# NO E-MAIL, graduates 2035, homeroom number 5
+
+# change constraints of email in students table to remove NOT NULL
+ALTER TABLE students
+ALTER COLUMN email 
+DROP NOT NULL
+
+# Insert Mark Watney
+INSERT INTO students(first_name, last_name, phone, graduation_year, homeroom_number)
+VALUES
+('Mark', 'Watney', 777-555-1234, 2035, 5)
+
+# Update (edit) Mark Watney phone number 
+UPDATE students
+SET phone = 7775551234
+RETURNING first_name, phone
+
+
+# Insert Teacher - Jonas Salk
+INSERT INTO teachers (first_name, last_name, homeroom_number, department, email, phone)
+VALUES
+('Jonas', 'Salk', 5, 'Biology', 'jsalk@school.org', 7775554321)
+
+
 
 # Conditional Expressions and Procedures ----
 
